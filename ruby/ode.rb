18d77cace637128f54f033a7f943724758c3d21d
Barney Livingston <barney@barnoid.org.uk>
2012-09-23 15:36:52 +0100

    ruby ode: dup the words out of the array to avoid applying modifications to both nouns if they happen to be the same

--- ruby/ode.rb
+++ ruby/ode.rb
@@ -9,20 +9,20 @@ verbs = File.open("verbs").readlines.map{ |k| k.chomp }
 adjs = File.open("adjs").readlines.map{ |k| k.chomp }
 ings = File.open("ings").readlines.map{ |k| k.chomp }
 
-noun1 = nouns.shuffle.first
+noun1 = nouns.shuffle.first.dup
 noun1.gsub!(/^\| /, "")
 
-verb = verbs.shuffle.first
+verb = verbs.shuffle.first.dup
 
-ing = ings.shuffle.first
+ing = ings.shuffle.first.dup
 
-adj = adjs.shuffle.first
+adj = adjs.shuffle.first.dup
 
 art = "a "
 art = "an " if adj[/^[aeiou]/i]
 art = "" if adj.to_i > 0
 
-noun2 = nouns.shuffle.first
+noun2 = nouns.shuffle.first.dup
 noun2.gsub!(/ \w+$/, "")
 art = "some " if noun2[/^(\w+) /, 1] == "some"
 noun2.gsub!(/^([^\s]+) /, "")
